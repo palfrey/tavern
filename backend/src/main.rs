9bc5cc8b64003d1b188f05b9e71002c6bcacfcbd
Tom Parker-Shemilt <palfrey@tevp.net>
2020-07-19 23:12:30 +0100

    Backend capable of serving static files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,18 +1,46 @@
 use actix_files::NamedFile;
-use actix_web::{HttpRequest, Result};
-use std::path::PathBuf;
+use actix_web::HttpRequest;
+use std::{env, fmt, io, path::PathBuf};
+use thiserror::Error;
+
+type Result<T> = std::result::Result<T, MyError>;
+
+#[derive(Debug, Error)]
+enum MyError {
+    Io {
+        #[from]
+        source: io::Error,
+    },
+    Env {
+        #[from]
+        source: env::VarError,
+    },
+    #[error(transparent)]
+    Other(#[from] anyhow::Error), // source and Display delegate to anyhow::Error
+}
+impl fmt::Display for MyError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:?}", self)
+    }
+}
+impl actix_web::error::ResponseError for MyError {}
 
 async fn index(req: HttpRequest) -> Result<NamedFile> {
-    let path: PathBuf = req.match_info().query("filename").parse().unwrap();
+    let mut path = PathBuf::from(env::var("FRONTEND")?);
+    let filename = match req.match_info().query("filename") {
+        "" => "index.html",
+        other => other,
+    };
+    path.push(filename);
     Ok(NamedFile::open(path)?)
 }
 
 #[actix_rt::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> io::Result<()> {
     use actix_web::{web, App, HttpServer};
 
     HttpServer::new(|| App::new().route("/{filename:.*}", web::get().to(index)))
-        .bind("127.0.0.1:5000")?
+        .bind("0.0.0.0:5000")?
         .run()
         .await
 }
